7fe8577e80b5ae7f63342119336090e529b5d9da
Ep Sooraj <epsooraj4@gmail.com>
2020-05-22 05:50:49 +0530

    Adding main website

---
+++ src/js/index.js
@@ -0,0 +1,66 @@
+import { hasClass, addClass, removeClass } from "./utils";
+import closeIcon from "../images/icons/zondicons/close.svg";
+import menuIcon from "../images/icons/zondicons/menu.svg";
+
+/*
+Event listener for Toggle Nav Menu
+*/
+
+document.querySelectorAll("[id=menulink]").forEach((item) => {
+	item.addEventListener("click", toggleMenu);
+});
+document.getElementById("toggleBtn").addEventListener("click", toggleMenu);
+
+// Toggles the menu and menu icon
+function toggleMenu(e) {
+	const togmenu = document.getElementById("togmenu");
+	if (hasClass(togmenu, "hidden")) {
+		removeClass(togmenu, "hidden");
+		// Change icon to close
+		document.getElementById("ham").src = closeIcon;
+	} else {
+		addClass(togmenu, "hidden");
+		// Change icon to hamburger
+		document.getElementById("ham").src = menuIcon;
+	}
+}
+
+/* Set copyright Year */
+document.getElementById("year").innerHTML = new Date().getFullYear() + "&nbsp;";
+
+/* Event Listener for show service arrow */
+
+document
+	.getElementById("show-service1")
+	.addEventListener("click", toggleServiceDesc);
+document
+	.getElementById("show-service2")
+	.addEventListener("click", toggleServiceDesc);
+document
+	.getElementById("show-service3")
+	.addEventListener("click", toggleServiceDesc);
+document
+	.getElementById("show-service4")
+	.addEventListener("click", toggleServiceDesc);
+document
+	.getElementById("show-service5")
+	.addEventListener("click", toggleServiceDesc);
+
+function toggleServiceDesc(event) {
+	const arrowdiv = event.currentTarget.id;
+	const arrowdivdom = document.getElementById(arrowdiv);
+	const servicedesc = "service-desc" + arrowdiv.charAt(arrowdiv.length - 1);
+	const servicedescdom = document.getElementById(servicedesc);
+	const downarrowicon = `<i class="fas fa-angle-double-down fa-lg"></i>`;
+	const uparrowicon = `<i class="fas fa-angle-double-up fa-lg"></i>`;
+
+	if (hasClass(servicedescdom, "service-card-desc-height")) {
+		removeClass(servicedescdom, "service-card-desc-height");
+		addClass(servicedescdom, "service-card-desc-height-auto");
+		arrowdivdom.innerHTML = uparrowicon;
+	} else {
+		removeClass(servicedescdom, "service-card-desc-height-auto");
+		addClass(servicedescdom, "service-card-desc-height");
+		arrowdivdom.innerHTML = downarrowicon;
+	}
+}
---
+++ tailwind.config.js
@@ -0,0 +1,31 @@
+module.exports = {
+	purge: [],
+	theme: {
+		extend: {
+			spacing: {
+				"36": "9rem",
+				"72": "18rem",
+				"84": "21rem",
+				"96": "24rem",
+				"screen-50": "50vh",
+				"screen-75": "75vh",
+				"screen-80": "80vh",
+				"screen/3": "calc(100vh / 3)",
+				"screen/4": "calc(100vh / 4)",
+				"screen/5": "calc(100vh / 5)",
+			},
+			fontFamily: {
+				body: ["Poppins"],
+				display: ["Open Sans"],
+			},
+			screens: {
+				xxl: "1500px",
+			},
+			colors: {
+				"near-black": "#010104",
+			},
+		},
+	},
+	variants: {},
+	plugins: [],
+};
